ba1c605ddbab77183c1b59540fbf9fb3d666c54e
김택진 <qlcdmlrlf@naver.com>
2025-12-03 03:12:07 +0900

    merge

--- z-com/src/app/layout.tsx
+++ z-com/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      >루트레이아웃
         {children}
       </body>
     </html>
